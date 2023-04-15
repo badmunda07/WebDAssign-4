16cdf4ddfac1ad906b84174a68d2fadaf559e704
badmunda07 <94034774+badmunda07@users.noreply.github.com>
2023-04-15 16:58:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.onkeydown = function(e){
+    console.log("Key code is: ", e.keyCode)
+    if(e.keyCode==38){
+        dino = document.querySelector('.dino ');
+        dino.classList.add('animateDino');
+        setTimeout(() => {
+          dino.classList.remove('animateDino')
+    }, 700);
+
+    }
+}
+setInterval(() => {
+    dino = document.querySelector('.dino');
+    gameOver = document.querySelector('.gameOver');
+    obstacle = document.querySelector('.obstacle');
+
+    dx = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(dino,null).getPropertyValue('top'));
+
+    ox = parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('left'));
+    oy = parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('top'));
+
+    offsetX = Math.abs(dx-ox);
+    offsetY = Math.abs(dy-oy);
+    console.log(offsetX, offsetY)
+    if(offsetX<93 && offsetY<52){
+        gameOver.style.visibility = 'visible'
+        obstacle.classList.remove = 'obstacleAni';
+
+    }
+}, 100);
